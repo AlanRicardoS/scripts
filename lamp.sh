6af6018f761297c4c482e6b31010313576288211
Alan Ricardo <50115877+AlanRicardoS@users.noreply.github.com>
2022-11-18 21:29:08 -0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-#######################################
-# Bash script to install an AMP stack and PHPMyAdmin plus tweaks. For Debian based systems.
-# Written by @AamnahAkram from http://aamnah.com
-
-# In case of any errors (e.g. MySQL) just re-run the script. Nothing will be re-installed except for the packages with errors.
-#######################################
-
 #COLORS
 # Reset
 Color_Off='\033[0m'       # Text Reset
@@ -18,16 +11,16 @@ Yellow='\033[0;33m'       # Yellow
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 
-
-sudo apt-get install php5-fpm
-sudo apt-get install phpmyadmin
 # Update packages and Upgrade system
 echo -e "$Cyan \n Updating System.. $Color_Off"
 sudo apt-get update -y && sudo apt-get upgrade -y
 
+sudo apt-get install php5-fpm
+sudo apt-get install phpmyadmin
+
 ## Install AMP
-echo -e "$Cyan \n Installing Apache2 $Color_Off"
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+#echo -e "$Cyan \n Installing Apache2 $Color_Off"
+#sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
 echo -e "$Cyan \n Installing PHP & Requirements $Color_Off"
 sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
