df40b0865a14aeea91c56acd81c3c2295dfc84a6
Alan Ricardo <50115877+AlanRicardoS@users.noreply.github.com>
2022-11-18 22:04:10 -0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -9,20 +9,20 @@ sudo apt install mysql-server -yq
 sudo apt install php libapache2-mod-php php-mysql -yq
 sudo mkdir /var/www/iac
 sudo chown -R $USER:$USER /var/www/iac
-sudo nano /etc/apache2/sites-available/iac.conf
-sudo a2ensite iac
-sudo a2dissite 000-default
-sudo apache2ctl configtest
-sudo systemctl reload apache2
-touch /var/www/iac/index.html
-echo '<html>
-  <head>
-    <title>your_domain website</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-
-    <p>This is the landing page of <strong>your_domain</strong>.</p>
-  </body>
-</html>' > /var/www/iac/index.html
+#sudo nano /etc/apache2/sites-available/iac.conf
+#sudo a2ensite iac
+#sudo a2dissite 000-default
+#sudo apache2ctl configtest
+#sudo systemctl reload apache2
+#touch /var/www/iac/index.html
+#echo '<html>
+#  <head>
+#    <title>your_domain website</title>
+#  </head>
+#  <body>
+#    <h1>Hello World!</h1>
+#
+#    <p>This is the landing page of <strong>your_domain</strong>.</p>
+#  </body>
+#</html>' > /var/www/iac/index.html
 
